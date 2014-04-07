674f668451972dd27d46da21ad1603135f1f1435
Martin J. Prochnow <email@martin-prochnow.de>
2014-04-07 21:12:04 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
-setup(name="mpddns server",
+setup(name="mpddns",
       version="0.8.0",
-      description="mpddns server daemon",
+      description="mpddns",
       long_description="A simple service to run your own dynamic DNS",
       author="Martin J. Prochnow",
       author_email="email@martin-prochnow.de",
